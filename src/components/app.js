23afff789d79b36da3434aedba8ec1ef5f05e06b
nebevets <steveben@exmsft.com>
2019-04-27 10:23:47 -0700

    feature set 1 complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,22 @@
+import { Route } from 'react-router-dom';
+import About from './about';
+import Contact from './contact';
+import Footer from './footer';
+import Header from './header';
+import Home from './home';
+import Services from './services';
 import React from 'react';
 import '../assets/css/app.scss';
 
 const App = () => (
-    <div>
-        <div className="app">
-            <h1>Sweet Corner</h1>
+    <div className="app">
+        <div className="container">
+            <Header/>
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route exact path="/" component={Home} />
+            <Route path="/services" component={Services} />
+            <Footer/>
         </div>
     </div>
 );
---
+++ src/components/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+  return(
+    <div>
+      <h1 className="center">Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
