a27320868b190ea9e39508d21e579f4b7f4d935e
nebevets <steveben@exmsft.com>
2019-04-29 11:32:38 -0700

    initial header html and css in place

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './header.scss';
+import headerImage from '../../assets/images/header.png';
+import logoImage from '../../assets/images/logo.png';
 
 const Header = () => {
   return(
-    <div>
-      <h1 className="center">This is the Header</h1>
+    <div className="header center">
+      <img src={headerImage} alt="topBorder" />
+      <ul className="headerNav">
+        <li><Link to="/">home</Link></li>
+        <li><Link to="/about">about us</Link></li>
+        <li><Link to="/services">services</Link></li>
+        <li><Link to="/contact">contact</Link></li>
+      </ul>
+      <img src={logoImage} alt="logo"/>
+      <div className="motto">
+        We deliver cupcakes for the important events in your life!
+      </div>
     </div>
   );
 }
