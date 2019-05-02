20453bef964882bf6f326c68fd79dc959744e68b
nebevets <steveben@exmsft.com>
2019-05-02 11:24:54 -0700

    made changes to reflect feature set 2 instructions

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
 import './header.scss';
+import React from 'react';
+import Nav from '../nav';
 import headerImage from '../../assets/images/header.png';
 import logoImage from '../../assets/images/logo.png';
 
@@ -8,12 +8,7 @@ const Header = () => {
   return(
     <div className="header center">
       <img src={headerImage} alt="topBorder" />
-      <ul className="headerNav">
-        <li><Link to="/">home</Link></li>
-        <li><Link to="/about">about us</Link></li>
-        <li><Link to="/services">services</Link></li>
-        <li><Link to="/contact">contact</Link></li>
-      </ul>
+      <Nav />
       <img src={logoImage} alt="logo"/>
       <div className="motto">
         We deliver cupcakes for the important events in your life!
